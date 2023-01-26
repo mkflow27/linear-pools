4a26957981571ac96fc9a8f07826a6adb4b2e328
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2023-01-26 11:08:50 +0100

    cleanup

--- packages/fork-tests/tests/20221130-midas-linear-pool/input.ts
+++ packages/fork-tests/tests/20221130-midas-linear-pool/input.ts
@@ -20,14 +20,12 @@ const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
 const BaseVersion = { version: 1, deployment: '20221130-midas-linear-pool' };
 
 export default {
-  goerli: {
-    Vault,
-    BalancerQueries,
-    ProtocolFeePercentagesProvider,
-    WETH,
-    FactoryVersion: JSON.stringify({ name: 'MidasLinearPoolFactory', ...BaseVersion }),
-    PoolVersion: JSON.stringify({ name: 'MidasLinearPool', ...BaseVersion }),
-    InitialPauseWindowDuration: MONTH * 3,
-    BufferPeriodDuration: MONTH,
-  },
+  Vault,
+  BalancerQueries,
+  ProtocolFeePercentagesProvider,
+  WETH,
+  FactoryVersion: JSON.stringify({ name: 'MidasLinearPoolFactory', ...BaseVersion }),
+  PoolVersion: JSON.stringify({ name: 'MidasLinearPool', ...BaseVersion }),
+  InitialPauseWindowDuration: MONTH * 3,
+  BufferPeriodDuration: MONTH,
 };
